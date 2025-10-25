445ea631b8ff970dd2294f509588cb06e49932a7
SachinNaiknaware <naiknawaresachin52@gmail.com>
2025-10-26 00:00:40 +0530

    25-10-2025

--- Pyhton file/250.py
+++ Pyhton file/250.py
@@ -154,4 +154,163 @@
 
 
 
-##
+##13 Comparig two Strings for ANAFRAMS
+# str1="Listen"
+# str2="Skinet"
+# str1=str1.replace("","").upper()
+# str2=str2.replace("","").upper()
+# if sorted(str1)==sorted(str2):
+#     print("True")
+# else:
+#     print("False")
+
+# str1="sachin"
+# str2="ahcins"
+# str1=str1.replace("","").upper()
+# str2=str2.replace("","").upper()
+# if sorted(str1) == sorted(str2) or len(str1) == len(str2):
+#     print("True")
+# else:
+#     print("False")
+
+##14 Checking For PALINDROME Using Extended Slicing Technique
+# str1= "kayak".lower() #A palindrome is a word, number, or phrase that reads the same forward and backward.
+# if str1 == str1[::-1]:
+#     print("True")
+# else:
+#     print("False")
+
+##15 Coutning white space in a Strig
+# str1="P S S S S"
+# print(str1.count(""))
+
+
+##16. Counting Digitd,Letters, and Spaces in string
+# import re
+# name = 'Python is 1'
+# digitcount  = re.sub("[0-9]","",name)
+# lettercount = re.sub("[^a-zA-Z]","",name)
+# spacecount  = re.findall("[\s]",name)
+
+# print(len(digitcount))
+# print(len(lettercount))
+# print(len(spacecount))
+
+# import re
+# name = 'Sachin is Engineer 1'
+# digitcount  =re.sub("[0-9]","",name)
+# lettercount =re.sub("[^a-zA-Z]","",name)
+# spacecount  =re.findall("[\s]",name) 
+
+# print(len(digitcount))
+# print(len(lettercount))
+# print(len(spacecount))
+
+
+##17. Counting special Characters in s string
+
+# def count_sp_car(string):
+#     sp_char= "!@#$%^&*()<>?/\[]:;~"
+#     count=0
+#     for i in string:
+#         if i in sp_char:
+#             count+=1
+#     return count
+# text='?'
+# result=count_sp_car(text)
+# print(result)
+
+# def sp_car(string):
+#     sp_char= "!@#$%^&*()<>?/\[]:;~"
+#     count= 0
+#     for i in string:
+#         if i in sp_char:
+#             count+=1
+#     return count
+
+# text="&"
+# result=sp_car(text)
+# print(result)
+
+##18 Removing all whitespace in a string
+# import re
+# string= "C O D E"
+# sp=re.compile(r"\s+")
+# result=re.sub(sp,"",string)
+# print(result)
+
+# string = "C O D E"
+# result = string.replace(" ", "")
+# print(result)
+
+
+# string = 'S F F E'
+# ress = string.replace(" ", "")
+# print(ress)
+
+
+##19 Building a Pyramid in Python
+# def Pyramid(n):
+#     for i in range(n):
+#         for j in range(i, n):
+#             print(" ",end="")
+#         for j in range(i+1):
+#             print("*",end="")
+#         for j in range(i):
+#             print("*",end="")
+#         print("")
+# Pyramid(5)
+
+# for i in range(10):
+#     print("*",end="")
+#     for j in range(i, 10):
+#         print("*",end="!")
+
+
+##20 Randomizing the items of a list in Python
+
+# from random import shuffle
+# lst=['python','is','easy']
+# shuffle(lst)
+# print(lst)
+#it will gernrate randomly
+
+
+
+##21 Create a generator to produce first n prime numbers
+
+# def isprime(num):
+#     for i in range(2, num):
+#         if num % i == 0:
+#             return False
+#     return True
+
+# def prime_generator(n):
+#     num = 2
+#     while n:
+#         if isprime(num):
+#             yield num
+#             n -= 1
+#         num += 1
+
+# x = int(input("Enter no. of prime numbers required: "))
+# it = prime_generator(x)
+# for e in it:
+#     print(e, end=" ")
+
+
+## write a program to check if a given number is prime or not
+# def prime_no(n):
+#     if n < 2:
+#         return False  # 0 and 1 are not prime
+#     for i in range(2, int(n**0.5) + 1):  # check up to sqrt(n)
+#         if n % i == 0:
+#             return False  # divisible by i → not prime
+#     return True  # no divisors found → prime
+
+# # Input and result
+# num = int(input("Enter a number :- "))
+# if prime_no(num):
+#     print(f"{num} is a prime number.")
+# else:
+#     print(f"{num} is not a prime number.")
---
+++ Pyhton file/re_module.py
@@ -0,0 +1,29 @@
+#re module 
+# The re module in python provide regular expration matching operations it's used for 
+
+#searching pattern in string 
+#replacing parts in string
+#spliting string based on patterns
+#validating formats based on email, phone numbers
+
+
+import re
+
+
+###1️⃣ Remove Digits
+import re
+name = 'My name is Sachin 57'
+# digitcount = re.sub("[0-29]", "", name)
+# print(len(digitcount))
+
+###2️⃣ Keep Only Letters
+# latter_count=re.sub("[^a-zA-Z]","",name)
+# print(len(latter_count))
+
+
+
+#3️⃣ Count Spaces
+# spacecount=re.findall("[\s]",name)
+# print(len(spacecount))
+
+
